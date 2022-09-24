5079d403c0eb426c20698953881afad0b98df8ce
frederickgon <yamilfedericogonzalez@gmail.com>
2022-09-24 15:47:28 -0300

    primera function

---
+++ back.js
@@ -0,0 +1,15 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+
+var pw = 3, pepe = 3;
+
+function toggleDesktopMenu() {
+    desktopMenu.classList.toggle('inactive');
+
+    if (pw == pepe)
+    {
+        console.log("penenennenennennenenennennnennenenne");
+    }
+}
