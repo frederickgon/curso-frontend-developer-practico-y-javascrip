30981025f89eec9c759b9b4d1c5c1a8a02940c6d
frederickgon <yamilfedericogonzalez@gmail.com>
2022-09-24 23:47:07 -0300

    la concha de la lora

--- back.js
+++ back.js
@@ -1,15 +1,14 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamicon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
-
-var pw = 3, pepe = 3;
+menuHamicon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive');
-
-    if (pw == pepe)
-    {
-        console.log("penenennenennennenenennennnennenenne");
-    }
+    desktopMenu.classList.toggle('inactive')
+}
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
