ccd6c5613370819671c44d7bece7a2d9be4d07a6
frederickgon <yamilfedericogonzalez@gmail.com>
2022-10-29 03:21:43 -0300

    cursopracticoterminado

--- back.js
+++ back.js
@@ -1,52 +1,70 @@
 const menuEmail = document.querySelector('.navbar-email');
-const menuHamicon = document.querySelector('.menu');
+const menuHamIcon = document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const productDetailCloseIcon = document.querySelector('.product-detail-close')
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const productDetailContainer = document.querySelector('#productDetail')
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
-menuHamicon.addEventListener('click', toggleMobileMenu);
-menuCarritoIcon.addEventListener('click', toggleCarritoshoppingCartContainer);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
 
 function toggleDesktopMenu() {
-    const isshoppingCartContainerClosed = shoppingCartContainer.classList.contains('inactive');
-    if (!isshoppingCartContainerClosed) {
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
+
+    if (!isAsideClosed) {
         shoppingCartContainer.classList.add('inactive');
     }
 
     desktopMenu.classList.toggle('inactive')
 }
-function toggleMobileMenu() {
-    const isshoppingCartContainerClosed = shoppingCartContainer.classList.contains('inactive');
 
-    if (!isshoppingCartContainerClosed) {
+function toggleMobileMenu() {
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
+
+    if (!isAsideClosed) {
     shoppingCartContainer.classList.add('inactive');
     }
+    closeProductDetailAside();
+
     mobileMenu.classList.toggle('inactive');
 }
-function toggleCarritoshoppingCartContainer() {
+function toggleCarritoAside() {
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
-    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
-    
+
     if (!isMobileMenuClosed){
-    mobileMenu.classList.add('inactive');
-    } 
-    if (!isDesktopMenuClosed){
-    desktopMenu.classList.add('inactive');
-    }
+        mobileMenu.classList.add('inactive');
+        } 
+
+        const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
+  
+        if (!isProductDetailClosed) {
+            productDetailContainer.classList.add('inactive'); 
+          }
+
 
     shoppingCartContainer.classList.toggle('inactive');
     
     
 }
+function openProductDetailAside(){
+    shoppingCartContainer.classList.add('inactive')
+    productDetailContainer.classList.remove('inactive');
+}
+
+function closeProductDetailAside() {
+    productDetailContainer.classList.add('inactive');
+  } 
 
 const productList = [];
 productList.push({
-    name: 'Bike',
-    price: 120,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    name: 'Gatito',
+    price: 999,
+    image: 'https://images.pexels.com/photos/13442289/pexels-photo-13442289.jpeg?cs=srgb&dl=pexels-ali-ramazan-%C3%A7ift%C3%A7i-13442289.jpg&fm=jpg',
 });
 productList.push({
     name: 'Pantalla LCD',
@@ -82,7 +100,8 @@ function renderProducts(arr) {
         //product= {name, price, image} -> product.image
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.image);
-    
+        productImg.addEventListener('click', openProductDetailAside);
+
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
     
